c8c420ebd5b27cef394db69e46837567a1cbcdc8
Onur <onur@instaon.io>
2020-10-14 22:18:09 +0300

    solve api issue

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,7 @@ import { BASE_URL } from "app_constants/api";
 import { API_KEY } from "app_config";
 
 export const sendRequest = async (path: string, params: object = {}) => {
-  const response = await axios.get(`${BASE_URL}/${path}?api_key=${API_KEY}&`, {
+  const response = await axios.get(`${BASE_URL}/${path}`, {
     params: {
       ...params,
       api_key: API_KEY,
