5283ba839f44194df5b95891366e33785a1297fc
Onur <onur@instaon.io>
2020-10-29 20:36:25 +0300

    try catch for image picker

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -5,12 +5,17 @@ import { IMAGE_ASPECT_RATIO, IMAGE_QUALITY } from "app_constants/ui";
 import { showCommonError } from "utils/popups";
 
 export const checkPermission = async () => {
-  const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
-  if (status !== PERMISSION_GRANTED) {
-    alert("Sorry, we need camera roll permissions to make this work!");
-    return false;
+  try {
+    const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (status !== PERMISSION_GRANTED) {
+      alert("Sorry, we need camera roll permissions to make this work!");
+      return false;
+    }
+    return true;
+  } catch (err) {
+    console.log("Image Picker Error: ", err);
+    showCommonError();
   }
-  return true;
 };
 
 export const pickImage = async () => {
