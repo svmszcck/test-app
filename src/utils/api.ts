3ec74eb8a0074f073808720059ad0d4dc55b001a
Onur <onur@instaon.io>
2020-10-29 20:15:46 +0300

    add try catch for api calls

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,11 +4,15 @@ import { BASE_URL } from "app_constants/api";
 import { API_KEY } from "app_config";
 
 export const sendRequest = async (path: string, params: object = {}) => {
-  const response = await axios.get(`${BASE_URL}/${path}`, {
-    params: {
-      ...params,
-      api_key: API_KEY,
-    },
-  });
-  return response?.data;
+  try {
+    const response = await axios.get(`${BASE_URL}/${path}`, {
+      params: {
+        ...params,
+        api_key: API_KEY,
+      },
+    });
+    return response?.data;
+  } catch (err) {
+    console.log("Api Error: ", err);
+  }
 };
